864df063275e4b5eec4f72c97bd940acf4346060
SungYu05 <tjddb0531@naver.com>
2022-09-28 17:08:18 +0900

    초기세팅완료

---
+++ src/main/java/site/metacoding/firstapp/FirstappApplication.java
@@ -0,0 +1,15 @@
+package site.metacoding.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Getter;
+
+@SpringBootApplication
+public class FirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstappApplication.class, args);
+	}
+
+}
