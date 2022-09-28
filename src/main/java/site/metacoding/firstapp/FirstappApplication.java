a46498304c6ecd7cdf757dd0311f78e85058ff8c
SungYu05 <tjddb0531@naver.com>
2022-09-28 17:33:50 +0900

    테이블 생성 완료

--- src/main/java/site/metacoding/firstapp/FirstappApplication.java
+++ src/main/java/site/metacoding/firstapp/FirstappApplication.java
@@ -3,8 +3,6 @@ package site.metacoding.firstapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.Getter;
-
 @SpringBootApplication
 public class FirstappApplication {
 
